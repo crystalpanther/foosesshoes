0b86416fb78b5216a9631f61a0d047ec9992fa13
Elina <elina.lilova@gmail.com>
2018-07-04 14:22:45 +0300

    added fonts and images

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+/**
+ * Created by elina on 7/4/2018.
+ */
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync'), // Подключаем Browser Sync
+    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    rename       = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
+
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch('app/**/**/*.css', ['css']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
+
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(gulp.dest('public/img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['img'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'app/css/**/style.css'
+    ])
+        .pipe(gulp.dest('public/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('public/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('public/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('public'));
+
+});
+
+
+gulp.task('default', ['watch']);
