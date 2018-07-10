57414576dae4ada63e454cb1a6a059c5282e53c2
Elina <elina.lilova@gmail.com>
2018-07-10 17:51:19 +0300

    add custom style for slick slider dots

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
+/**
+ * MAIN BANNER SLICK SLIDER CONTROLLER
+ */
 $('.banner__slider').slick({
     arrows: false,
     slidesToShow: 1,
@@ -7,22 +10,27 @@ $('.banner__slider').slick({
     dotsClass: 'banner__dots'
 });
 var dots = $('.banner__dots');
+dots.find('button').text(''); //remove numbers content from dots
+dots.find('li').addClass('banner__dot'); //add class for dot
 
-dots.find('li').addClass('banner__dot');
-dots.find('li').html(
+var i = 0
+$('.banner__dot').each(function () {
+    i++;
+    $(this).attr('id', 'dot'+i);
+}); //add id for every dot in dots
+dots.find('#dot1').html(
+    '<div class="banner__line"> <span class="banner__inline"></span></div>'+
+    '<div class="banner__title">Pink Shoes</div>'+
+    '<div class="banner__text">Now af $145,99</div>'
+); //add html content to first dot
+dots.find('#dot2').html(
+    '<div class="banner__line"> <span class="banner__inline"></span></div>'+
+    '<div class="banner__title">Anna Field</div>'+
+    '<div class="banner__text">Limited Edition</div>'
+); //add html content to second dot
+dots.find('#dot3').html(
     '<div class="banner__line"> <span class="banner__inline"></span></div>'+
     '<div class="banner__title">Prada</div>'+
     '<div class="banner__text">Summer is coming</div>'
-);
-dots.find('button').text('');
-dots.find('button').addClass('banner__line');
-dots.find('.banner__dot ').each(function() {
-    if($(this).hasClass('slick-active')) {
-        $(this).find('.banner__inline').addClass('banner__inline-active');
-        $(this).find('.banner__title').addClass('banner__title-active');
-        $(this).find('.banner__text').addClass('banner__text-active');
-    }
-    else {
-        $(this).find('.banner__inline').removeClass('banner__inline-active');
-    }
-});
+); //add html content to third dot
+dots.find('button').addClass('banner__line'); //add custom class to dot button
