ed0c2eca701922ab93cac9c7c2b2e299a4ca11f2
Elina <elina.lilova@gmail.com>
2018-07-13 11:59:34 +0300

    added footer items, added sass folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,20 @@
  * Created by elina on 7/4/2018.
  */
 var gulp         = require('gulp'),
+    sass =         require('gulp-sass'),
     browserSync  = require('browser-sync'), // Подключаем Browser Sync
-    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    concat       = require('gulp-concat'),
+    del          = require('del'),
+    autoprefixer = require('gulp-autoprefixer')
     rename       = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
 
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
@@ -18,20 +28,21 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 
 gulp.task('watch', ['browser-sync'], function() {
-    gulp.watch('app/css/**/.css', ['css']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
-
+gulp.task('clean', function() {
+    return del.sync('public'); // Удаляем папку dist перед сборкой
+});
 
 gulp.task('img', function() {
     return gulp.src('app/img/**/*') // Берем все изображения из app
         .pipe(gulp.dest('public/img')); // Выгружаем на продакшен
 });
 
-gulp.task('build', ['img'], function() {
-
-    var buildCss = gulp.src('app/css/**/*')
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+    var buildCss = gulp.src('app/css/*.css')
         .pipe(gulp.dest('public/css'))
 
     var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
